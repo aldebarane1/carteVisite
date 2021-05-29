35a88ffb11710146d377576db2dd1066634fa540
aldebarane1 <matriculemat@gmail.com>
2021-05-29 21:32:28 +0200

    new menu

--- lib/main.dart
+++ lib/main.dart
@@ -44,19 +44,19 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
-      'Index 0: Home',
+      'Index 0: Présentation',
       style: optionStyle,
     ),
     Text(
-      'Index 1: Business',
+      'Index 1: Compétences',
       style: optionStyle,
     ),
     Text(
-      'Index 2: School',
+      'Index 2: Formations',
       style: optionStyle,
     ),
     Text(
-      'Index 3: Settings',
+      'Index 3: CV',
       style: optionStyle,
     ),
   ];
@@ -81,7 +81,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
-            backgroundColor: Colors.red,
+            backgroundColor: Colors.orangeAccent,
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.business),
@@ -91,12 +91,12 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           BottomNavigationBarItem(
             icon: Icon(Icons.school),
             label: 'School',
-            backgroundColor: Colors.purple,
+            backgroundColor: Colors.blueAccent,
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
             label: 'Settings',
-            backgroundColor: Colors.pink,
+            backgroundColor: Colors.black12,
           ),
         ],
         currentIndex: _selectedIndex,
